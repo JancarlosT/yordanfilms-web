cb96b3b70e531d3ad875d63d7e352a51ffe4b46b
JancarlosT <jancarlotejada01@gmail.com>
2026-08-09 17:28:54 -0400

    Agregar gunicorn, flask-mail y dotenv; usar puerto dinamico

--- backend/app.py
+++ backend/app.py
@@ -198,4 +198,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         seed_data()
-    app.run(debug=True, port=5000,)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
